51643ef1456be72dfdf91ff3f75b56eb293e88fa
salah2000albakri <salahalbakri55@gmail.com>
2022-08-29 00:22:22 +0200

    add asteroids

--- main.py
+++ main.py
@@ -6,12 +6,17 @@ window = pyglet.window.Window(width=1280, height=960)
 
 player = image.load("./assets/img/starship.png")
 
+asteroid_big = image.load("./assets/img/Asteroid_Large.png")
+asteroid_medium = image.load("./assets/img/Asteroid_Medium.png")
+asteroid_small = image.load("./assets/img/Asteroid_Small.png")
 
 @window.event
 def on_draw():
     window.clear()
     player.blit(50, 50)
-
+    asteroid_big.blit(100,50)
+    asteroid_medium.blit(150,30)
+    asteroid_small.blit(50,100)
 
 def update(dt):
     pass
